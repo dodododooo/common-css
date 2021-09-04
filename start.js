cee63877dede9ac65af3c3448eaf9d3b0c2ce891
gongzheng <meme_456@163.com>
2021-09-04 10:25:38 +0800

    ss

--- start.js
+++ start.js
@@ -1,21 +1,40 @@
 const sass = require('node-sass');
 const fs = require('fs');
-const dist_file = process.env.NODE_ENV == 'production' ? 'common-css.css' : 'common-css-dev.css';
-const outputStyle = process.env.NODE_ENV == 'production' ? 'compressed' : 'expanded';
-sass.render(
+const pruduction = process.env.NODE_ENV == 'production';
+sass.renderSync(
   {
     file: './main.scss',
-    outputStyle,
+    outputStyle: 'expanded',
   },
   function (err, result) {
     if (!err) {
-      fs.writeFile(dist_file, result.css, function (err) {
+      fs.writeFile('common-css-dev.css', result.css, function (err) {
         if (!err) {
-          //file written on disk
+          console.log(err);
         }
       });
     } else {
-      console.log(err)
+      console.log(err);
     }
   }
 );
+
+if (pruduction) {
+  sass.renderSync(
+    {
+      file: './main.scss',
+      outputStyle: 'compressed',
+    },
+    function (err, result) {
+      if (!err) {
+        fs.writeFile('common-css.css', result.css, function (err) {
+          if (!err) {
+            console.log(err);
+          }
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+}
